76c668422461592bb3c4ab1e95860a7d8e37d477
ty <3272577027@qq.com>
2025-12-11 18:40:15 +0800

    update format

--- src/Game.hpp
+++ src/Game.hpp
@@ -10,34 +10,34 @@
 class Game
 {
 public:
-    Game();
+  Game();
 
-    bool init();
-    void run();
+  bool init();
+  void run();
 
 private:
-    void processEvents();
-    void update(float dt);
-    void render();
-    void checkCollisions();
-    void spawnEnemy();
-    void resetGame();
+  void processEvents();
+  void update(float dt);
+  void render();
+  void checkCollisions();
+  void spawnEnemy();
+  void resetGame();
 
-    // 配置（放在前面以便初始化列表使用）
-    const unsigned int m_screenWidth = 1280;
-    const unsigned int m_screenHeight = 720;
-    const float m_shootCooldown = 0.3f;
-    const float m_bulletSpeed = 500.f;
+  // 配置（放在前面以便初始化列表使用）
+  const unsigned int m_screenWidth = 1280;
+  const unsigned int m_screenHeight = 720;
+  const float m_shootCooldown = 0.3f;
+  const float m_bulletSpeed = 500.f;
 
-    sf::RenderWindow m_window;
-    std::unique_ptr<Tank> m_player;
-    std::vector<std::unique_ptr<Enemy>> m_enemies;
-    BulletManager m_bulletManager;
+  sf::RenderWindow m_window;
+  std::unique_ptr<Tank> m_player;
+  std::vector<std::unique_ptr<Enemy>> m_enemies;
+  BulletManager m_bulletManager;
 
-    sf::Texture m_bulletTexture;
+  sf::Texture m_bulletTexture;
 
-    sf::Clock m_clock;
-    sf::Clock m_shootClock;
+  sf::Clock m_clock;
+  sf::Clock m_shootClock;
 
-    bool m_gameOver = false;
+  bool m_gameOver = false;
 };
